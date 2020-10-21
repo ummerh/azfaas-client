40c23399f08100af7ac5e1a3fdbdce9cfd9836d6
ummerh <harsha.madathil@gmail.com>
2020-10-21 20:49:14 +0000

    base version

---
+++ src/main/java/com/jet/demo/faas/client/simple/HomeController.java
@@ -0,0 +1,45 @@
+package com.jet.demo.faas.client.simple;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@Value("${app.home.url}")
+	private String appUrl;
+
+	@RequestMapping("/")
+	public ModelAndView home(HttpServletRequest req) {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("app.home.url", appUrl);
+		if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null
+				&& SecurityContextHolder.getContext().getAuthentication().getPrincipal() != null) {
+			Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			if (OidcUser.class.isAssignableFrom(principal.getClass())) {
+				OidcUser oidUser = (OidcUser) principal;
+				String givenName = oidUser.getClaimAsString("name");
+				mv.addObject("USER_GIVEN_NAME", givenName);
+				req.getSession().setAttribute("USER_GIVEN_NAME", givenName);
+			}
+		}
+		return mv;
+	}
+
+	@Autowired
+	@RequestMapping(value = "/oauth2/code/azure", method = RequestMethod.POST)
+	public ModelAndView kioskPost() {
+		ModelAndView mv = new ModelAndView("home");
+		mv.addObject("app.home.url", appUrl);
+		return mv;
+	}
+
+}
